779d920bc9a8690d0ed46e92c5f2c015cb3f4b44
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2023-03-12 12:44:29 +0530

    existing codes are changed

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 import store from "./redux/store";
 import { Provider } from "react-redux";
